c124ae4b928521bfc5b49d92ddc150dd1d875a62
Ravikiran-Dev18 <ravikirandalawai@gmail.com>
2026-04-24 12:14:47 +0530

    Second changes

--- src/com/mycodes/Pen.java
+++ src/com/mycodes/Pen.java
@@ -7,11 +7,11 @@ public class Pen {
 	}
 	
 	public static void main(String[] args) {
-		Pen p1= new Pen();
-		System.out.println(p1.color);
+		Pen p3= new Pen();
+		System.out.println(p3.color);
 		
 		Pen p2= new Pen();
-		System.out.println(p2.color);
+		System.out.println(p.color);
 	}
 
 }
